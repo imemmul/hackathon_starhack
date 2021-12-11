6769fcc44d0a324cd7c2c7790ddf61fddbe36b64
Emir Ulurak <emirulurak@Emirs-MacBook-Air-2.local>
2021-12-11 22:19:59 +0300

    va improvement

--- va.py
+++ va.py
@@ -9,14 +9,29 @@ def speak(text):
     engine.runAndWait()
     engine.stop()
 
+def callback(recognizer, audio):
+    try:
+        said = recognizer.recognize_google(audio).lower()
+        if 'hey' in said:
+            speak("I am listening.")
+            command = get_audio()
+            if 'order' in command:
+                speak("What do you want to order ?")
+                command = get_audio()
+                if "pizza" in command:
+                    speak("I like pizza")
+    except:
+        pass
+
+
 def get_audio():
     r = sr.Recognizer()
+    text = ''
     with sr.Microphone() as source:
-    # OSX may use 48KHz instead of 44KHz ?
         print("Say something!")
         try:
             audio = r.listen(source, phrase_time_limit=2)
-            print(r.recognize_google(audio))
+            return (r.recognize_google(audio).lower)
         except TypeError as e:
             print("error; {0}".format(e))
 
@@ -32,5 +47,10 @@ def callback(recognizer, audio):
 
 
 if __name__ =="__main__":
-    #get_audio()
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    with m as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
+    print("--- BACKGROUND LISTENING HAS BEEN STARTED ---")
+    stop_listening = r.listen_in_background(m, callback=callback)
     pass
