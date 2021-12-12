4d9a7887c8c7a8a6224db8ba7823592e88616abf
Emir Ulurak <emirulurak@Emirs-MacBook-Air-2.local>
2021-12-12 04:07:02 +0300

    i did some

--- csv_writer.py
+++ csv_writer.py
@@ -1,16 +1,22 @@
 import csv
-csv_columns = ['order_id', 'order_type', 'latitude', 'longtitude']
+csv_columns_car = ['order_id', 'order_type', 'latitude', 'longtitude']
 csv_columns_store = ['name', 'latitude', 'longtitude', 'duration']
 
-#dict_data = [{'order_id': 1, 'order_type': 'food', 'latitude': 10, 'longtitude': 20}]
+#dict_car = [{'order_id': 1, 'order_type': 'food', 'latitude': 10, 'longtitude': 20}]
 dict_store = [{'name':'Starbucks', 'latitude':30.5, 'longtitude':50.5, 'duration':0.5}, {'name':'Burger', 'latitude':20.5, 'longtitude':35.5, 'duration':35}]
 
-csv_file = "data_stores.csv"
-try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns_store)
-        writer.writeheader()
-        for data in dict_store:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+
+def write_csv(filename):
+    csv_file = filename
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns_store)
+            writer.writeheader()
+            for data in dict_store:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+
+if __name__ == "__main__":
+    write_csv()
--- va.py
+++ va.py
@@ -1,9 +1,15 @@
 import pyttsx3
 import speech_recognition as sr
+import csv
+
 
 engine = pyttsx3.init()
 
-temp_stores = []
+selected_order_type = {'food_type': None, 'food_kind': None, 'drink': None}
+dict_car = {'order_id': 1, 'order_type': None, 'latitude': 10, 'longtitude': 20}
+dict_store = [{'name':'Starbucks', 'latitude':30.5, 'longtitude':50.5, 'duration':0.5}, {'name':'Burger', 'latitude':20.5, 'longtitude':35.5, 'duration':35}]
+csv_columns_store = ['name', 'latitude', 'longtitude', 'duration']
+csv_columns_car = ['order_id', 'order_type', 'latitude', 'longtitude']
 
 half_response = 'Okay, i took your order i will check whether it is available.'
 
@@ -13,46 +19,58 @@ def speak(text):
     engine.runAndWait()
     engine.stop()
 
-def command_loop():
+def command_loop_drink():
+    order = get_audio()
+    if 'coke' or 'cola' in order:
+        speak(half_response)
+        selected_order_type['drink'] = 'cola'
+    elif 'sprite' in order:
+        speak(half_response)
+        selected_order_type['drink'] = 'sprite'
+
+def command_loop_food():
     try:
         speak("What do you want to order ?")
         order = get_audio()
         if "pizza" in order:
             speak("How do you want your pizza ?")
-            order = get_audio()
+            selected_order_type['food_type'] = 'pizza'
             if 'pepperoni' in order:
                 speak("Okay, do you want any drink ?")
-                order = get_audio()
-                if 'coke' or 'cola' in order:
-                    speak(half_response)
-                elif 'sprite' in order:
-                    speak(half_response)
+                selected_order_type['food_kind'] = 'pepperoni'
+                command_loop_drink()
             elif 'margarita' in order:
                 speak("Okay, do you want any drink ?")
-                order = get_audio()
-                if 'coke' or 'cola' in order:
-                    speak(half_response)
-                elif 'sprite' in order:
-                    speak(half_response) 
-
-            
-            
-        
+                selected_order_type['food_kind'] = 'margarita'
+                command_loop_drink()
     except:
         pass
 
+def save_order_type():
+    dict_car['order_type'] = selected_order_type
+    write_csv('data_car.csv')
+
+def write_csv(filename):
+    csv_file = filename
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns_store)
+            writer.writeheader()
+            for data in dict_store:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
 
 def get_audio():
     r = sr.Recognizer()
-    text = ''
     with sr.Microphone() as source:
         print("Say something!")
         try:
-            audio = r.listen(source, phrase_time_limit=2)
+            audio = r.listen(source, phrase_time_limit=1)
             return (r.recognize_google(audio).lower())
         except TypeError as e:
             print("error; {0}".format(e))
 
 
 if __name__ =="__main__":
-    command_loop()
+    command_loop_food()
